cc0e6a5254b0f6268088201f08a77ac36bc9bcb3
ConradKash <kakurucon1234@gmail.com>
2025-06-27 17:06:03 +0300

    Slider is done

--- components/HeroSection/Slideshow.tsx
+++ components/HeroSection/Slideshow.tsx
@@ -3,7 +3,11 @@ import Image from "next/image";
 
 import TextButton from "../Buttons/TextButton";
 import styles from "./Hero.module.css";
-
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 // swiperjs
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -56,65 +60,68 @@ const Slideshow = () => {
           spaceBetween={0}
           loop={true}
           autoplay={{
-            delay: 5000,
-            disableOnInteraction: false,
+        delay: 5000,
+        disableOnInteraction: false,
           }}
           navigation={true}
           pagination={{
-            clickable: true,
-            type: "fraction",
-            dynamicBullets: true,
+        clickable: true,
+        type: "fraction",
+        dynamicBullets: true,
           }}
           className="mySwiper"
         >
           {sliders.map((slider) => (
-            <SwiperSlide key={slider.id}>
-              <div className="hidden lg:block">
-                <Image
-                  layout="responsive"
-                  src={slider.image}
-                  width={1144}
-                  height={572}
-                  alt={"some name"}
-                />
-              </div>
-              <div className="hidden sm:block lg:hidden">
-                <Image
-                  layout="responsive"
-                  src={slider.imageTablet}
-                  width={820}
-                  height={720}
-                  alt={"some name"}
-                />
-              </div>
-              <div className="sm:hidden">
-                <Image
-                  layout="responsive"
-                  src={slider.imageMobile}
-                  width={428}
-                  height={800}
-                  alt={"some name"}
-                />
-              </div>
-              <div
-                className={
-                  slider.rightText
-                    ? styles.rightTextSection
-                    : styles.leftTextSection
-                }
-              >
-                <span className={styles.subtitle}>{slider.subtitle}</span>
-                <span
-                  className={`${styles.title} text-center ${
-                    slider.rightText ? "sm:text-right" : "sm:text-left"
-                  }`}
-                >
-                  {slider.titleUp} <br />
-                  {slider.titleDown}
-                </span>
-                <TextButton value={t("shop_now")} />
-              </div>
-            </SwiperSlide>
+        <SwiperSlide key={slider.id}>
+          <div className="hidden lg:block relative">
+            <Image
+          layout="responsive"
+          src={slider.image}
+          width={1144}
+          height={572}
+          alt={"some name"}
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+          </div>
+          <div className="hidden sm:block lg:hidden relative">
+            <Image
+          layout="responsive"
+          src={slider.imageTablet}
+          width={820}
+          height={720}
+          alt={"some name"}
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+          </div>
+          <div className="sm:hidden relative">
+            <Image
+          layout="responsive"
+          src={slider.imageMobile}
+          width={428}
+          height={800}
+          alt={"some name"}
+            />
+            <div className="absolute inset-0 bg-black bg-opacity-30"></div>
+          </div>
+          <div
+            className={
+          slider.rightText
+            ? styles.rightTextSection
+            : styles.leftTextSection
+            }
+          >
+            <span className={`${styles.subtitle} drop-shadow-lg`}>{slider.subtitle}</span>
+            <span
+          className={`${styles.title} text-center drop-shadow-lg ${
+            slider.rightText ? "sm:text-right" : "sm:text-left"
+          }`}
+            >
+          {slider.titleUp} <br />
+          {slider.titleDown}
+            </span>
+            <TextButton value={t("shop_now")} />
+          </div>
+        </SwiperSlide>
           ))}
         </Swiper>
       </div>
